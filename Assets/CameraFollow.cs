805687abddd0335c7d8499243b51fd2d50fb2b9c
Yamen Alamer <ulquiorracero1@gmail.com>
2026-04-24 21:23:30 +0300

    New Features

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -7,24 +7,32 @@ public class CameraFollow : MonoBehaviour
 
     [HideInInspector]
     public Vector3 currentFollowPos;
+
     private Vector3 offset;
+    private CameraLean cameraLean;
 
     void Start()
     {
         offset = transform.position - target.position;
         currentFollowPos = transform.position;
+        cameraLean = GetComponent<CameraLean>();
     }
 
     void LateUpdate()
     {
         Vector3 desired = target.position + offset;
-        currentFollowPos = Vector3.Lerp(currentFollowPos, desired, smoothSpeed * Time.deltaTime);
+        currentFollowPos = Vector3.Lerp(
+            currentFollowPos, desired,
+            smoothSpeed * Time.deltaTime);
 
-        // Apply shake on top of follow position
         Vector3 shakeOffset = ScreenShake.Instance != null
             ? ScreenShake.Instance.GetShakeOffset()
             : Vector3.zero;
 
-        transform.position = currentFollowPos + shakeOffset;
+        Vector3 leanOffset = cameraLean != null
+            ? cameraLean.GetLeanOffset()
+            : Vector3.zero;
+
+        transform.position = currentFollowPos + shakeOffset + leanOffset;
     }
 }
